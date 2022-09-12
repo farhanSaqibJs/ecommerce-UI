aa0dbe0504281c8d2d5f2237a0caec4ed10583a2
FarhanSaqibBaloch <farhan.saqib@cheetay.pk>
2022-09-12 09:51:59 +0500

    register.html UI

--- js/main.js
+++ js/main.js
@@ -53,3 +53,21 @@ $(document).resize(function() {
 tabParentHeight();
 
 // product Details Table Js End
+
+
+// register form animation JS start
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('form-container');
+
+signUpButton.addEventListener('click', () => {
+  container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+  container.classList.remove("right-panel-active");
+});
+
+
+
+// register form animation JS End
